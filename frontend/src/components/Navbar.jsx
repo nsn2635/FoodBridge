f951ff36c8073708c809f1b50456a891093ba8e9
Priyanshu0980 <rajp200704@gmail.com>
2026-06-14 00:05:43 +0530

    Frontend part done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -18,20 +18,22 @@ const Navbar = () => {
         <h2>🥗 Food Rescue</h2>
       </div>
       
-      <div className="nav-links">
+      <div className="nav-center">
         <Link to="/">Home</Link>
         <Link to="/analytics">Impact</Link>
-        
+      </div>
+
+      <div className="nav-right">
         {!token ? (
           <>
-            <Link to="/login" className="nav-link">Login</Link>
+            <Link to="/login" className="btn-ghost small-btn">Login</Link>
             <Link to="/register" className="btn-primary small-btn">Register</Link>
           </>
         ) : (
           <>
-            {role === 'Donor' && <Link to="/donor">Dashboard</Link>}
-            {role === 'NGO' && <Link to="/ngo">Dashboard</Link>}
-            <button onClick={handleLogout} className="btn-outline-danger">Logout</button>
+            {role === 'Donor' && <Link to="/donor" className="nav-link">Dashboard</Link>}
+            {role === 'NGO' && <Link to="/ngo" className="nav-link">Dashboard</Link>}
+            <button onClick={handleLogout} className="btn-outline-danger small-btn">Logout</button>
           </>
         )}
       </div>
